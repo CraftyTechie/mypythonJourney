e1b49a187ea1cd2554c243876162da0d4768cf9a
CraftyTechie <abbyjepkosgei6@gmail.com>
2026-02-16 01:00:23 +0300

    print function

--- basics.py
+++ basics.py
@@ -2,5 +2,12 @@
 name = "abigael" #string
 age = 23 #number
 height = 5.4 #decimal
-is_Student = True #boolean
+# is_Student = True #boolean
+
+
+#print function
+print("hello world")
+print("my age is: ",age)
+print(name,"'s age is",age)
+
 
